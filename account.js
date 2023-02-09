7c561d63f3f6d7ee832f3bf77664103f6d9ae385
zhiyanwong <s10247154@connect.np.edu.sg>
2023-02-09 20:28:22 +0800

    login testing

---
+++ account.js
@@ -0,0 +1,23 @@
+  function auth(event) {
+    event.preventDefault();
+  
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // check if the username of password are empty
+    if (username.length === 0 || password.length === 0) {
+      alert("Username and password must be filled!");
+      return false;
+    }
+  
+    if (username === "123" && password === "123") {
+      window.location.replace("home.html");
+    } else {
+      alert("Please enter valid information");
+      return;
+    }
+  }
+  
+  
+
+  
